f8afa450da7dff5f82ea4939382dbee33b5ab14e
Ubuntu <ubuntu@ip-172-31-43-63.ap-northeast-1.compute.internal>
2021-05-20 11:24:56 +0000

    新しい用語を追加する関数を追加（関数名だけ）

--- app/controllers/word_dictionaries_controller.rb
+++ app/controllers/word_dictionaries_controller.rb
@@ -3,4 +3,7 @@ class WordDictionariesController < ApplicationController
         @word = WordDictionary.find_by(word: params[:word])
         @other_words = WordDictionary.where('word like ? AND word != ?', "%#{params[:word]}%", params[:word]).limit(10)
     end
+
+    def create
+    end
 end
